75bfaa3422a02bbef5c9836bcd85e3647b92ed1a
Alex <a.stashkevich@psu.by>
2021-02-19 16:46:37 +0300

    lesson 3

--- app/src/main/java/dev/alexstashkevich/firsttest/MainActivity.kt
+++ app/src/main/java/dev/alexstashkevich/firsttest/MainActivity.kt
@@ -3,11 +3,13 @@ package dev.alexstashkevich.firsttest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import dev.alexstashkevich.firsttest.databinding.ActivityMainBinding
-import dev.alexstashkevich.firsttest.utils.showToast
+import com.wolfram.alpha.WAEngine
+import com.wolfram.alpha.WAPlainText
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG by lazy { "wolfram" }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -18,8 +20,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.topAppBar)
 
-        binding.searchButton.setOnClickListener {
-            showToast(binding.questionInput.text.toString())
-        }
+        binding.searchButton.setOnClickListener { askWolfram(binding.questionInput.text.toString()) }
+    }
+
+    private fun askWolfram(question: String) {
+        val wolframAppId = "YOUR API KEY"
+
+        val engine = WAEngine()
+        engine.appID = wolframAppId
+        engine.addFormat("plaintext")
+
+        val query = engine.createQuery()
+        query.input = question
+
+        binding.answerOutput.text = getString(R.string.let_me_think)
+
+        Thread {
+            val queryResult = engine.performQuery(query)
+
+            binding.answerOutput.post { // Возвращаемся на Main Thread
+                when {
+                    queryResult.isError -> {
+                        Log.e(TAG, queryResult.errorMessage)
+                        binding.answerOutput.text = queryResult.errorMessage
+                    }
+                    !queryResult.isSuccess -> {
+                        Log.e(TAG, "Sorry, I don't understand, can you rephrase?")
+                        binding.answerOutput.text = getString(R.string.sorry)
+                    }
+                    else -> {
+                        queryResult.pods
+                            .asSequence()
+                            .filterNot { it.isError }
+                            .forEach {
+                                it.subpods.forEach { subpod ->
+                                    subpod.contents.forEach { element ->
+                                        if (element is WAPlainText) {
+                                            Log.d(TAG, element.text)
+                                            binding.answerOutput.text = element.text
+                                        }
+                                    }
+                                }
+                            }
+                    }
+                }
+            }
+        }.start()
     }
 }
